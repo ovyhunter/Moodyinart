5cc762c6018accd854d4f734a77fe0728f351e4f
Paul Chelaru <paul5175@stud.kea.dk>
2018-05-26 22:18:45 +0200

    about page done

--- js/script.js
+++ js/script.js
@@ -1,29 +1,28 @@
+// About Page
 function fetchData() {
-    fetch("http://www.paulchelaru.com/wp-json/wp/v2/about")
-    .then( e => e.json())
-    .then(showData)
+    fetch("http://www.paulchelaru.com/wp-json/wp/v2/about?_embed")
+        .then(e => e.json())
+        .then(showData)
 }
 
-
-function showData(data){
+function showData(data) {
     console.log(data);
     data.forEach(showSinglePost);
 }
 
 function showSinglePost(aPost) {
-    console.log()
+    console.log(aPost._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url);
+    console.log(aPost._embedded["wp:featuredmedia"][0].media_details);
     let template = document.querySelector("#about_template").content;
     let clone = template.cloneNode(true);
     clone.querySelector(".about_title").textContent = aPost.title.rendered;
     clone.querySelector(".about_p").innerHTML = aPost.content.rendered;
+
+    clone.querySelector(".about_img").setAttribute("src", aPost._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url);
     
     let section = document.querySelector("#about");
     section.appendChild(clone);
-    
+
 }
 
 fetchData()
-
-
-
-
